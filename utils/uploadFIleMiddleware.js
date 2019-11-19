472659ee90b68a5bdaa90358d664355ad6f85e3c
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-11-19 20:43:41 +0700

    Customized the filename of GridFS.

--- utils/uploadFIleMiddleware.js
+++ utils/uploadFIleMiddleware.js
@@ -6,12 +6,10 @@ const url = process.env.MONGODB_SERVER_DEV;
 const storage = new GridFsStorage({
     url,
     options: {useNewUrlParser: true, useUnifiedTopology: true},
-    filename: function (req, file, cb) {
-        const datetimestamp = Date.now();
-        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
-    },
-    metadata: function(req, file, cb) {
-        cb(null, { originalname: file.originalname });
+    file: (req, file) => {
+        return {
+            filename: 'paperflix_' + Date.now()
+        }
     },
     root: 'avatars'
 })
