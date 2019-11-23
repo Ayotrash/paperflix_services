dce8401f66bd749e471874ee8cfbde4612547c03
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-11-23 09:00:11 +0700

    Setup testing environment with Mocha & Chai.

---
+++ test/test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./app.js');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('AUTHENTICATION', function () {
+    describe('/register', function () {
+        it("")
+    })
+})
