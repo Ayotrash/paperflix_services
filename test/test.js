095068d1110b492a23b70b2baaf2a1d3508707cd
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-11-23 10:34:37 +0700

    Added test for app.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('./app.js');
+const server = require('../app.js');
+const faker = require('faker');
 
 const should = chai.should();
 
@@ -9,6 +10,25 @@ chai.use(chaiHttp);
 
 describe('AUTHENTICATION', function () {
     describe('/register', function () {
-        it("")
+        const body = {
+            firstname: faker.name.firstname,
+            lastname: faker.name.lastname,
+            email: faker.internet.email,
+            password: faker.internet.password,
+            gender: "female",
+            avatar: faker.random.image,
+            device_name: faker.lorem.word,
+            device_id: faker.random.uuid
+        }
+        it("Should be statusCode: 201", done => {
+            chai.request(server)
+                .post('/v1/register')
+                .send(body)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    console.log("Response Body: ", res)
+                })
+            done()
+        })
     })
 })
